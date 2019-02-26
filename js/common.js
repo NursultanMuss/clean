14b9ce8ab5192de60fa9f672a4ae8685ddce6c91
NursultanMuss <nursalga@gmail.com>
2019-02-26 18:03:16 +0600

    8 урок 15 минута

--- js/common.js
+++ js/common.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
+	$(".s_portfolio li").click(function () {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+	$('.portfolio_items').mixItUp();
 	$('.popup').magnificPopup({type:'image'});
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
 	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
 	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
+	$(".left .s_services_item").animated("fadeInLeft", "fadeOutLeft");
 	$(".animation_1").animated("flipInY", "flipOutY");
 	$(".animation_3").animated("fadeInRight", "fadeOutRight");
+	$(".right .s_services_item").animated("fadeInRight", "fadeOutRight");
 	function heightDetect(){
 		$(".main_head").css("height", $(window).height())
 	}
